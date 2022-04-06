e4728424e8d350bee14f1bf94aaef568ac86198d
Sheldon <sheldonaldridge@yahoo.com>
2022-04-06 12:03:54 +0200

    Update bubble-animation.js

--- js/bubble-animation.js
+++ js/bubble-animation.js
@@ -1,3 +1,7 @@
+/****************************************************************
+ * Fade animation for speech bubble with 1.5s delay before start*
+ ****************************************************************/
+
 let bubble = document.getElementById("speach-bubble");
 let noDisplay; bubble.style.display ="none";
 
@@ -8,8 +12,21 @@ function bubbleAnimate()
     noDisplay = bubble.style.display ="block";
     bubble.style.animation="slidein 0.5s 1";
 }
- 
 
+/****************************************************************
+ * changes text in bubble with name input                       *
+ ****************************************************************/
+
+let welcome ="Welcome to your To-Do App"
+let text;
+function changetext()
+{
+    document.getElementById("userName").onsubmit = function changedtext()
+    {
+        text = document.getElementById("nameId");
+        document.getElementById("speach-bubble").innerHTML = welcome + text.value;
+    }
+}
 
  
     
