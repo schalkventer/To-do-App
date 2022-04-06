b8d9ae9ad1ac64697c1c6909bb001088e182dc78
Sheldon <sheldonaldridge@yahoo.com>
2022-04-06 21:28:01 +0200

    Update dateTime.js

--- js/dateTime.js
+++ js/dateTime.js
@@ -1,4 +1,18 @@
-let days =["Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday","Sunday"];
-let getDatetime = document.getElementById("dateTime");
 
-getDatetime = new date();
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+
+  setTimeout(startTime, 1000);
+
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
