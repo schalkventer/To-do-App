01401ec0fdf610766bb8ebe24186fe806e9004cf
Sheldon <sheldonaldridge@yahoo.com>
2022-04-06 12:03:48 +0200

    Update submit-animation.js

--- js/submit-animation.js
+++ js/submit-animation.js
@@ -1,11 +1,4 @@
-let welcome ="Welcome to your To-Do App"
+//import{bubbleAnimate} from '../js/bubble-animation.js';
 
-windows.onload = function()
-{
-    document.getElementById("userName").onsubmit = function()
-    {
-        let text = document.getElementById("nameId");
-        let changebox = document.getElementById("speach-bubble").innerHTML = welcome + " " + text;
-    }
-}
+
 
