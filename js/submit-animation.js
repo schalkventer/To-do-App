103b2b3d503c018bb2497964dadb1fed7e6eecfd
Sheldon <sheldonaldridge@yahoo.com>
2022-04-08 15:51:59 +0200

    Update submit-animation.js

--- js/submit-animation.js
+++ js/submit-animation.js
@@ -1,4 +1,30 @@
-//import{bubbleAnimate} from '../js/bubble-animation.js';
+import{bubbleAnimate} from '../js/bubble.js';
 
+/****************************************************************
+ * changes text in bubble with name input                       *
+ ****************************************************************/
+
+
+ let form = document.querySelector('.form');
+
+ form.addEventListener('click', event =>{
+ 
+     event.preventDefault();
+ 
+     if(event.target.className === "submitBtn")
+     {
+ 
+     }
+ 
+ })
+ 
+ function changetext(text)
+ {
+     
+     {
+         text = document.getElementById("nameId");
+         document.getElementById("speach-bubble").innerHTML = welcome + text.value;
+     }
+ }
 
 
