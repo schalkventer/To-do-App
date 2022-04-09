56c08558bb2b4a2f2e9e0680d4490a351b0dd5d9
Sheldon <sheldonaldridge@yahoo.com>
2022-04-09 06:09:26 +0200

    Update bubbleChanged.js
    added bubbleAnimate(textChange) call back

--- js/bubbleChanged.js
+++ js/bubbleChanged.js
@@ -1,18 +1,20 @@
 import{bubbleAnimate} from '../js/bubble.js';
 
-
 let form = document.querySelector('.form')
-
 
 form.addEventListener('submit' , (e) => {
     
     e.preventDefault();
 
   const input = document.getElementById('input').value;
-  let textChnage = document.getElementById('speach-bubble');
+  let textChange = document.getElementById('speach-bubble');
 
-  textChnage.innerHTML = "Welcome to your To-do app \n" + input;
-  
+  textChange.innerHTML = "Welcome to your To-do app \n" + input;
+  bubbleAnimate(textChange)
 });
 
-bubbleAnimate()
+bubbleAnimate(textChange)
+{
+    textChnage.style.display ="block";
+    textChnage.style.animation="slidein 1.6s 1";
+}
